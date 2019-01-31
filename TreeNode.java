ce68f0c3e50cde8a605c0fa31de664e2487e91b6
flowingrain <shlip@foxmail.com>
2019-01-31 17:41:02 +0800

    Update TreeNode.java
    值改为int型

--- TreeNode.java
+++ TreeNode.java
@@ -4,5 +4,10 @@ public class TreeNode {
     Integer val;
     TreeNode left;
     TreeNode right;
-    TreeNode(Integer x) { val = x; }
+    //TreeNode(Integer x) { val = x; }
+    TreeNode(int x) {
+        val = x;
+        left=null;
+        right=null;
+    }
 }
