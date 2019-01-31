8c7e4ce5fcd49aeb86ff272fbe13f00a51c40365
flowingrain <shlip@foxmail.com>
2019-01-31 17:41:41 +0800

    Update Tree.java
    分层打印二叉树，父子节点关系可视化

--- Tree.java
+++ Tree.java
@@ -1,14 +1,12 @@
 package LeetCode;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Tree {
     public boolean isPowerSumOf2(int num){
         int pow=0;
         while(num>0){
             num-=Math.pow(2,pow);//Math.pow(a,b)，a的b次方
-            //System.out.println(num);
             pow++;
         }
         if(num==0)
@@ -18,11 +16,16 @@ public class Tree {
     }
     public TreeNode makeBinaryTreeByArray(Integer[] array,int index){
         if(index<array.length){
-            TreeNode t=new TreeNode(array[index]);
-            array[index]=0;
-            t.left=makeBinaryTreeByArray(array,index*2+1);
-            t.right=makeBinaryTreeByArray(array,index*2+2);
-            return t;
+            TreeNode t;
+            if(array[index]!=null){//index范围内也可以为null
+                t=new TreeNode(array[index]);
+                array[index]=0;
+                t.left=makeBinaryTreeByArray(array,index*2+1);
+                t.right=makeBinaryTreeByArray(array,index*2+2);
+                return t;
+            }
+            else
+                return null;
         }else
             return null;
     }
@@ -32,7 +35,77 @@ public class Tree {
     public TreeNode buildTree(Integer[] array){
         return makeBinaryTreeByArray(array,0);
     }
-    public void printTree(TreeNode root){
+
+    public String printStack(Stack stack){
+        StringBuilder sb=new StringBuilder();
+        Stack temp1=(Stack)stack.clone();
+        while(!temp1.isEmpty()){
+            TreeNode tn=(TreeNode)temp1.pop();
+            if(tn.val!=null)
+                sb.append(tn.val+"\t");
+        }
+        return sb.reverse().toString();
+    }
+
+    public String printTree(TreeNode root){
+        StringBuilder tree = new StringBuilder();
+        List level = new ArrayList();
+        List<List<TreeNode>> wholeTree = new ArrayList<List<TreeNode>>();
+        level.add(root);
+        while(level.size() > 0){
+            wholeTree.add(level);
+            List newLevel = new ArrayList();
+            for (int i = 0; i < level.size(); i++){
+                TreeNode node = (TreeNode) level.get(i);//节点可以为空。针对非全二叉树
+                if(node!=null){
+                    newLevel.add(node.left);
+                    newLevel.add(node.right);
+                }
+            }
+            level = newLevel;
+        }
+        int height = wholeTree.size();
+        for (int i = 0; i < height; i++){
+            level = wholeTree.get(i);
+            int tabNum = (int)(Math.pow(2, height - i - 1) - 1);
+            for (int j = 0; j < tabNum; j++){
+                tree.append("\t");
+            }
+            if(level.get(0)!=null)
+                tree.append(((TreeNode)level.get(0)).val+"\t") ;
+            else
+                tree.append("\t");
+            tabNum = (int) (Math.pow(2, height - i) - 1);
+            for (int j = 1; j < level.size(); j++){
+                for (int k = 0; k < tabNum; k++){
+                    tree.append("\t");
+                }
+                if(level.get(j)!=null)
+                    tree.append(((TreeNode)level.get(j)).val+"\t");
+                else
+                    tree.append("\t");
+            }
+            tree.append("\n");
+        }
+        System.out.println(tree);
+        return tree.toString();
+    }
+
+    public void DFS(TreeNode root){
+        Stack stack=new Stack();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node=(TreeNode)stack.pop();
+            System.out.println(node.val);
+            if(node.val!=null&&node.left==null&&node.right==null)//叶子节点
+                printStack(stack);
+            if(node.left!=null)
+                stack.push(node.right);
+            if(node.right!=null)
+                stack.push(node.left);
+        }
+    }
+   /* public void printTree(TreeNode root){//输出包含层次结构和相对位置
         int count=0;
         if(root == null){
             return;
@@ -52,9 +125,9 @@ public class Tree {
             if(isPowerSumOf2(count))
                 System.out.println();
         }
-    }
-/*    public static void main(String[] args){
+    }*/
+    public static void main(String[] args){
         Tree tree=new Tree();
         System.out.println(tree.isPowerSumOf2(4));
-    }*/
+    }
 }
