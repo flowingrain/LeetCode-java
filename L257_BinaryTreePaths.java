b8be763894712490c88d3fc948e8cbe28d740609
flowingrain <shlip@foxmail.com>
2019-02-07 14:31:01 +0800

    Update L257_BinaryTreePaths.java
    使用递归获得各叶子节点的路径

--- L257_BinaryTreePaths.java
+++ L257_BinaryTreePaths.java
@@ -1,69 +1,26 @@
 package LeetCode;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class L257_BinaryTreePaths {
-    public List<String> binaryTreePaths(TreeNode root) {//解答里每次都加入answer参数是为了保证对同一个链表操作
-        List<String> answer= new LinkedList<String>();
-        LinkedList stack=new LinkedList();
-        LinkedList<TreeNode> dfsOrder=new LinkedList<TreeNode>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode node=(TreeNode)stack.pop();
-            if(node!=null){
-                dfsOrder.add(node);
-                stack.push(node.right);
-                stack.push(node.left);
-            }
+   public List<String> binaryTreePaths(TreeNode root) {//使用递归
+        List<String> paths= new LinkedList<String>();
+        if(root==null)
+            return paths;
+        if(root.left==null&&root.right==null)//根节点为叶子节点
+            paths.add(String.valueOf(root.val));
+        for(String path:binaryTreePaths(root.left)){
+            paths.add(root.val+"->"+path);
         }
-        Iterator<TreeNode> it = dfsOrder.iterator();
-        boolean[] isLeaf=new boolean[dfsOrder.size()];//标记节点是否为叶子节点
-        while(it.hasNext()){
-            TreeNode tn=it.next();//tn一定不为null
-            int cur;//当前叶子节点在链表中的位置
-            if(tn==null)
-                continue;
-            else if(tn.left==null&&tn.right==null){//叶子节点
-                StringBuilder sb=new StringBuilder();
-                cur=dfsOrder.indexOf(tn);//叶子节点的index
-                LinkedList<TreeNode> temp=new LinkedList<TreeNode>(dfsOrder);
-                isLeaf[cur]=true;
-                for(int i=cur-1;i>=0;i--)//从前向后找叶子节点，从后向前找叶子节点的路径
-                {
-                    if(temp.get(i)==null){
-                        continue;
-                    } else if(temp.get(i).left==temp.get(cur)||temp.get(i).right==temp.get(cur))//当前节点的父节点
-                    {
-                        cur=i;
-                    }
-                    else//不是下一个节点的父节点，不在当前叶子节点的路径上.可能是叶子节点
-                    {
-                        temp.set(i,null);
-                    }
-                }
-                Iterator<TreeNode> pointer = temp.iterator();
-                while(pointer.hasNext()){
-                    TreeNode tn1=pointer.next();
-                    if(tn1!=null) {
-                        if (tn1 != tn) {
-                            sb.append(tn1.val+"->");
-                        } else{
-                            sb.append(tn.val);//打印到叶子节点，后边不打印
-                            break;
-                        }
-                    }
-                }
-                answer.add(sb.toString());
-            }
-            else
-                continue;
+        for(String path:binaryTreePaths(root.right)){
+            paths.add(root.val+"->"+path);
         }
-        return answer;
+        return paths;
     }
     public static void main(String[] args){
         L257_BinaryTreePaths l257=new L257_BinaryTreePaths();
-        Integer[] nums={1,2,3,null,5};
+        Integer[] nums={37,-34,-48,null,-100,-100,48,null,null,null,null,-54,null,-71,-22,null,null,null,8};
+        //Expected ["37->-34->-100","37->-48->-100","37->-48->48->-54->-71","37->-48->48->-54->-22->8"]
         Tree tree=new Tree();
         TreeNode root=tree.buildTree(nums);
         //tree.printTree(root);
