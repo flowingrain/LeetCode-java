a77525db5129c605de5996f3f83ea2b048b5d56d
flowingrain <shlip@foxmail.com>
2019-02-23 15:33:03 +0800

    Update L289_GameOfLife.java
    原地修改二维数组，

--- L289_GameOfLife.java
+++ L289_GameOfLife.java
@@ -9,38 +9,31 @@ public class L289_GameOfLife {
         {
             for(int col=Math.max(j-1,0);col<=Math.min(board[0].length-1,j+1);col++)//列
             {
-                /*if((row!=i||col!=j)&&board[row][col]==1){
-                    count++;
-                }*/
                 count+=board[row][col]&1;//board[row][col]==1时board[row][col]&1==1，board[row][col]==0时board[row][col]&1==0
             }
         }
         //对board[i][j]进行了一次统计
         return count-(board[i][j]&1);
     }
-    public void gameOfLife(int[][] board) {
+    public void gameOfLife(int[][] board) {//也可以使用Board的最高位存储当前状态，低位存储存活邻居数
         int rowNum=board.length;
         int colNum=board[0].length;
-        if(board==null||rowNum==0||colNum==0)
+        if(rowNum==0||colNum==0)
             return;
         for(int i=0;i<rowNum;i++)
         {
             for(int j=0;j<colNum;j++)
             {
                 int liveNeighbor=liveNeighbors(board,i,j);
-                if(board[i][j]==1){
-                    if(liveNeighbor<2)
-                        next[i][j]=0;
-                    else if(liveNeighbor==2||liveNeighbor==3)
-                        next[i][j]=1;
-                    else if(liveNeighbor>3)
-                        next[i][j]=0;
-                }else{
-                    if(liveNeighbor==3)
-                        next[i][j]=1;
-                }
+                if(((board[i][j]&1)==1)&&(liveNeighbor==2||liveNeighbor==3)){
+                    /*if(liveNeighbor<2||liveNeighbor>3)
+                        board[i][j]=1;//当前1 下一步0。此时board[i][j]不需要改变
+                    else //(liveNeighbor==2||liveNeighbor==3)
+                        board[i][j]=3;//当前1 下一步1*/
+                        board[i][j]=3;
+                }else if(((board[i][j]&1)==0)&&(liveNeighbor==3))//当前0 下一步1。毕竟与上边的if不是独立的。
+                    board[i][j]=2;
             }
-            //System.out.println();
         }
         for(int i=0;i<rowNum;i++)
         {
@@ -49,10 +42,8 @@ public class L289_GameOfLife {
                 board[i][j]=board[i][j]>>1;
             }
         }
-       /* for(int i=0;i<next.length;i++) {
-            System.out.println(Arrays.toString(board[i]));
-        }*/
     }
+
     public static void main(String[] args){
         L289_GameOfLife l289=new L289_GameOfLife();
         int[][] input= {{0,1,0},
